444be169acb0318c4b576b1773d90089d7a0fab6
dsvalenciah <dsvalenciah@unal.edu.co>
2018-09-15 21:54:28 -0500

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='marshmallowjson',
-    version='0.4.4',
+    version='0.4.5',
     description="Turns a JSON type definition into a marshmallow validation and serialization toolkit.",
     long_description=readme + '\n\n' + history,
     author="Oscar David Arbeláez",
